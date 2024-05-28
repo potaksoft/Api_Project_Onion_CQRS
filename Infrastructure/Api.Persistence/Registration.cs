5c69ced2b7d926d6a499dd00c8a418fa345be18f
potak <107770088+potak91@users.noreply.github.com>
2024-05-28 08:46:24 +0300

    implemented WriteRepoistory

--- Infrastructure/Api.Persistence/Registration.cs
+++ Infrastructure/Api.Persistence/Registration.cs
@@ -18,6 +18,7 @@ namespace Api.Persistence
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
         }
     }
 }
