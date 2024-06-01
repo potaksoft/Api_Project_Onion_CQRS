ff7e7cc8016557f1e67ff6d7fbb469a7360cef23
potak <107770088+potak91@users.noreply.github.com>
2024-06-01 18:02:52 +0300

    Organize Fluent Validation PipeLine

--- Core/Api.Application/Features/Products/Commands/DeleteProduct/DeleteProductCommandRequest.cs
+++ Core/Api.Application/Features/Products/Commands/DeleteProduct/DeleteProductCommandRequest.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Api.Application.Features.Products.Commands.DeleteProduct
 {
-    public class DeleteProductCommandRequest:IRequest
+    public class DeleteProductCommandRequest:IRequest<Unit>
     {
         public int Id { get; set; }
     }
