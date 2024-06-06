922dc0547630cd27a28dc74c423168583b54365f
potak <107770088+potak91@users.noreply.github.com>
2024-06-06 17:47:30 +0300

    Implemented IdentityDbContext Created User and Role Entities

--- Infrastructure/Api.Persistence/Registration.cs
+++ Infrastructure/Api.Persistence/Registration.cs
@@ -1,5 +1,6 @@
 ﻿using Api.Application.Interfaces.Repositories;
 using Api.Application.Interfaces.UnitOfWorks;
+using Api.Domain.Entities;
 using Api.Persistence.Context;
 using Api.Persistence.Repositories;
 using Api.Persistence.UnitOfWorks;
@@ -22,6 +23,18 @@ namespace Api.Persistence
             services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
             services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddIdentityCore<User>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 2;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireDigit = false;
+                opt.SignIn.RequireConfirmedEmail = false;
+            })
+                .AddRoles<Role>()
+                .AddEntityFrameworkStores<AppDbContext>();
         }
     }
 }
