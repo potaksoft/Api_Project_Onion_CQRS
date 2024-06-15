6da4228499c93616d5a22cdb31c42c564e1f3cf7
potak <107770088+potak91@users.noreply.github.com>
2024-06-15 22:31:53 +0300

    Tested EfCore,Redis,MSSQL performance with 1 milion Data

---
+++ Core/Api.Application/Features/Brands/Queries/GetAllBrands/GetAllBrandQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Api.Application.Bases;
+using Api.Application.Interfaces.AutoMappers;
+using Api.Application.Interfaces.UnitOfWorks;
+using Api.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Application.Features.Brands.Queries.GetAllBrands
+{
+    public class GetAllBrandQueryHandler : BaseHandler,IRequestHandler<GetAllBrandQueryRequest,IList<GetAllBrandQueryResponse>>
+    {
+        public GetAllBrandQueryHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor contextAccessor) : base(mapper, unitOfWork, contextAccessor)
+        {
+        }
+
+        public async Task<IList<GetAllBrandQueryResponse>> Handle(GetAllBrandQueryRequest request, CancellationToken cancellationToken)
+        {
+            var brands = await _unitOfWork.GetReadRepository<Brand>().GetAllAsync();
+
+            return _mapper.Map<GetAllBrandQueryResponse,Brand>(brands);
+        }
+    }
+}
